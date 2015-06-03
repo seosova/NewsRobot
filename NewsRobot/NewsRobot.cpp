cd59b1ce6547052f14ca3b350a6fa464649a50e8
Denis Savenko <seosova@gmail.com>
2015-06-04 01:25:01 +0300

    Work read from csv to vector string. Done all news fields. Read and show.

--- NewsRobot/NewsRobot.cpp
+++ NewsRobot/NewsRobot.cpp
@@ -15,24 +15,24 @@
 using namespace std;
 class ReadCSV {
 protected:
-	vector<string> m_date;
+	vector<string> m_csv_date;
 public:
 	void ReadRow(istream& str) {	/// взято с http://stackoverflow.com/questions/1120140/how-can-i-read-and-parse-csv-files-in-c 
 		string line;
 		getline(str, line);
 		stringstream lineStream(line);
 		string cell;
-		m_date.clear();
+		m_csv_date.clear();
 		while (getline(lineStream, cell, '\t'))
 		{
-			m_date.push_back(cell);
+			m_csv_date.push_back(cell);
 		}
 	}
 	size_t size() const {
-		return m_date.size();
+		return m_csv_date.size();
 	}
 	string const& operator[](size_t index) const {
-		return m_date[index];
+		return m_csv_date[index];
 	}
 
 
@@ -43,46 +43,41 @@ istream& operator>>(istream& str, ReadCSV& date)
 	date.ReadRow(str);
 	return str;
 }
-class NewsAnalitic {
-
-};
 class News {
 protected:
-	int m_date_year;
-	int m_date_month;
-	int m_date_day;
-	int m_date_hour;
-	int m_date_minute;
-
-	char* m_currency;
-	char* m_description;
-	char* m_impact;
-	char* m_actual;
-	char* m_forecast;
-	char* m_previous;
-
+	vector<string> m_date;
+	vector<string> m_currency;
+	vector<string> m_description;
+	vector<string> m_impact;
+	vector<string> m_actual;
+	vector<string> m_forecast;
+	vector<string> m_previous;
 public:
 	News() {}
 	~News() {}
-	void SetDateYear(int c_date_year) { m_date_day = c_date_year; }
-	void SetDateMonth(int c_date_month) { m_date_month = c_date_month; }
-	void SetDateDay(int c_date_day) { m_date_day = c_date_day; }
-	void SetDateHour(int c_date_hour) { m_date_hour = c_date_hour; }
-	void SetDateMinute(int c_date_minute) { m_date_minute = c_date_minute; }
-	void SetCurrency(char* c_currency) { m_currency = c_currency; }
-	void SetDescription(char* c_description) { m_description = c_description; }
-	void SetImpact(char* c_impact) { m_impact = c_impact; }
-	void SetActual(char* c_actual) { m_actual = c_actual; }
-	void SetForecast(char* c_forecast) { m_forecast = c_forecast; }
-	void SetPrevious(char* c_previous) { m_previous = c_previous; }
+
+	void SetDate(string c_date) { m_date.push_back(c_date); }
+	void SetCurrency(string c_currency) { m_currency.push_back(c_currency); }
+	void SetDescription(string c_description) { m_description.push_back(c_description); }
+	void SetImpact(string c_impact) { m_impact.push_back(c_impact); }
+	void SetActual(string c_actual) { m_actual.push_back(c_actual); }
+	void SetForecast(string c_forecast) { m_forecast.push_back(c_forecast); }
+	void SetPrevious(string c_previous) { m_previous.push_back(c_previous); }
+
+	void ShowDate(int i) { cout << m_date[i] << endl; }
+	void ShowCurrency(int i) { cout << m_currency[i] << endl; }
+	void ShowDescription(int i) { cout << m_description[i] << endl; }
+	void ShowImpact(int i) { cout << m_impact[i] << endl; }
+	void ShowActual(int i) { cout << m_actual[i] << endl; }
+	void ShowForecast(int i) { cout << m_forecast[i] << endl; }
+	void ShowPrevious(int i) { cout << m_previous[i] << endl; }
 };
 
 class RobotControll {
 protected:
 	char* m_input_news_file = 0;
 	char m_input_statistic_file = 0;
-//	News m_current_news;
-
+	News m_input_news;
 public:
 	void SetNewsFile(char* c_input_news_file) { c_input_news_file = m_input_news_file; }
 	void SetStatisticFile(char c_input_statistic_file) { c_input_statistic_file = m_input_statistic_file; }
@@ -91,12 +86,26 @@ public:
 };
 
 void RobotControll::ReadNewsFile() {
-	ifstream	file(m_input_news_file);
+	ifstream	file("News_2007_2015.csv");
 	ReadCSV Row;
 	while (file >> Row)
 	{
-		cout << "4th Element(" << Row[3] << ")\n";
+		m_input_news.SetDate(Row[0]);
+		m_input_news.SetCurrency(Row[1]);
+		m_input_news.SetDescription(Row[2]);
+		m_input_news.SetImpact(Row[3]);
+		m_input_news.SetActual(Row[4]);
+		m_input_news.SetForecast(Row[5]);
+		m_input_news.SetPrevious(Row[6]);
+
 	}
+	m_input_news.ShowDate(10);
+	m_input_news.ShowCurrency(10);
+	m_input_news.ShowDescription(10);
+	m_input_news.ShowImpact(10);
+	m_input_news.ShowActual(10);
+	m_input_news.ShowForecast(10);
+	m_input_news.ShowPrevious(10);
 }
 
 int main() {
@@ -106,4 +115,5 @@ int main() {
 
 	return 0;
 }
+
 
